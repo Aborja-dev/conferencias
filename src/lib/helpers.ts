44e7b98bcf96c841a5a5003c00b01a468388bde1
ankahara <hecatombe100@gmail.com>
2025-07-30 02:46:07 -0600

    trabajar en la pagina de calendario

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,1 +1,5 @@
 export const formatDate = (date: number) => new Date(date).toLocaleDateString();
+export const getDateFormated = (date: number) => ({
+    date: new Date(date).toLocaleDateString(),
+    hour: new Date(date).toLocaleTimeString()
+});
